690a1cb30d709be88127b142ae8f2544873fdb66
panchadijaya <panchadijaya15@yahoo.co.id>
2023-03-15 17:12:51 +0700

    Update coinkey-util.test.js

--- test/coinkey-util.test.js
+++ test/coinkey-util.test.js
@@ -5,7 +5,7 @@ var util = require('../lib/util')
 
 describe('util', function () {
   describe('normalizeVersions()', function () {
-    it('should normalize the versions object (i.e. be compatible with coininfo)', function () {
+    it('should normalize the versions object (i.e. be compatible with mubdiinfo)', function () {
       assert.deepEqual(util.normalizeVersions({ public: 1, private: 2 }), { public: 1, private: 2 })
       assert.deepEqual(util.normalizeVersions({ versions: { public: 1, private: 2 } }), { public: 1, private: 2 })
       assert.deepEqual(util.normalizeVersions({ version: { public: 1, private: 2 } }), { public: 1, private: 2 })
